ea88b91c7e320c9e2ec4ef689a68ee382d1cf802
christopher <christopher@christopher-PC>
2015-01-04 17:06:52 +0100

    Add all files

---
+++ NoMesseges/src/li/inc/messages/messages.java
@@ -0,0 +1,28 @@
+package li.inc.messages;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class messages  extends JavaPlugin implements Listener
+{
+	public void onEnable()
+	{
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onPlayerJoin(PlayerJoinEvent event)
+	{
+		event.setJoinMessage("");
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onPlayerQuit(PlayerQuitEvent event)
+	{
+		event.setQuitMessage("");
+	}
+}
